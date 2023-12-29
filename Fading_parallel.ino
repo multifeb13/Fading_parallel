cf5bd1b8478516d33fdb6edfbaa0d3580881ad5c
multifeb13 <musicfair@gmail.com>
2023-12-29 11:09:03 +0900

    [feat] lint with google style

--- Fading_parallel.ino
+++ Fading_parallel.ino
@@ -1,13 +1,13 @@
 int led_pin = 1;
 
-double getLEDValue(const double freq, const double time){
+double getLEDValue(const double freq, const double time) {
   double val_sin = sin(TWO_PI * (freq / 2.0f) * time);
   double val_abs = abs(val_sin);
 
   return val_abs;
 }
 
-void analogOutput(const pin_size_t pinNumber, const double value){
+void analogOutput(const pin_size_t pinNumber, const double value) {
   // move value 0.0-1.0 to 0-255
   int value_in_analog = int(value * 255);
   analogWrite(pinNumber, value_in_analog);
@@ -21,7 +21,7 @@ void setup() {
 void loop() {
   double freq = random(1, 50) / 10.0f;
 
-  for(double time=0; time<7; time+=0.01){
+  for (double time = 0; time < 7; time += 0.01) {
     double value = getLEDValue(freq, time);
     analogOutput(led_pin, value);
 
